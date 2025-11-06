7f83328192eb418fdc42d601d39e0dc61c805d8d
Moon Awan <malikmoon.developer061@gmail.com>
2025-11-06 18:51:36 +0500

    Improve admin UI responsiveness and mobile sidebar

--- Frontend/src/components/AdminSideBar.jsx
+++ Frontend/src/components/AdminSideBar.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   LayoutDashboard,
   LayoutList,
   LogOut,
+  Menu,
   Package,
   SquareArrowOutUpRight,
+  SquareX,
   Users,
 } from "lucide-react";
 import { useNavigate } from "react-router";
@@ -13,88 +15,125 @@ import { useAuth } from "../contexts/AuthContext";
 const AdminSideBar = () => {
   const navigate = useNavigate();
   const { logout } = useAuth();
+  const [isSidebar, setIsSidebar] = useState(false);
 
   return (
-    <div className="admin-sidebar">
-      <span className="admin-sidebar-header">
+    <>
+      <div className="admin-mobile-topbar">
+        <span
+          className="icon"
+          onClick={() => {
+            setIsSidebar(!isSidebar);
+          }}
+        >
+          <Menu />
+        </span>
         <h2
           onClick={() => {
             navigate("/admin/dashboard");
           }}
         >
-          Admin Panel
+          Cart Sense
         </h2>
-        <span onClick={() => navigate("/")} className="icon">
+        <span
+          className="icon"
+          onClick={() => {
+            navigate("/");
+          }}
+        >
           <SquareArrowOutUpRight />
         </span>
-      </span>
-      <div className="admin-sidebar-list-items">
-        <div
-          onClick={() => {
-            navigate("/admin/dashboard");
-          }}
-          className={
-            location.pathname === "/admin/dashboard"
-              ? "admin-sidebar-active"
-              : ""
-          }
-        >
-          <span className="icon">
-            <LayoutDashboard />
+      </div>
+      <div className={`admin-sidebar ${isSidebar ? "show-admin-sidebar" : ""}`}>
+        <span className="admin-sidebar-header">
+          <h2
+            onClick={() => {
+              navigate("/admin/dashboard");
+            }}
+          >
+            Admin Panel
+          </h2>
+          <span onClick={() => navigate("/")} className="icon go-site">
+            <SquareArrowOutUpRight />
           </span>
-          Dashboard
+          <span
+            onClick={() => setIsSidebar(false)}
+            className="icon close-admin-sidebar"
+          >
+            <SquareX />
+          </span>
+        </span>
+        <div className="admin-sidebar-list-items">
+          <div
+            onClick={() => {
+              navigate("/admin/dashboard");
+              setIsSidebar(false);
+            }}
+            className={
+              location.pathname === "/admin/dashboard"
+                ? "admin-sidebar-active"
+                : ""
+            }
+          >
+            <span className="icon">
+              <LayoutDashboard />
+            </span>
+            Dashboard
+          </div>
+          <div
+            onClick={() => {
+              navigate("/admin/orders"); setIsSidebar(false);
+            }}
+            className={
+              location.pathname === "/admin/orders"
+                ? "admin-sidebar-active"
+                : ""
+            }
+          >
+            <span className="icon">
+              <LayoutList />
+            </span>
+            Orders
+          </div>
+          <div
+            onClick={() => {
+              navigate("/admin/products"); setIsSidebar(false);
+            }}
+            className={
+              location.pathname === "/admin/products"
+                ? "admin-sidebar-active"
+                : ""
+            }
+          >
+            <span className="icon">
+              <Package />
+            </span>
+            Products
+          </div>
+          <div
+            onClick={() => {
+              navigate("/admin/users"); setIsSidebar(false);
+            }}
+            className={
+              location.pathname === "/admin/users" ? "admin-sidebar-active" : ""
+            }
+          >
+            <span className="icon">
+              <Users />
+            </span>
+            Users
+          </div>
         </div>
-        <div
-          onClick={() => {
-            navigate("/admin/orders");
-          }}
-          className={
-            location.pathname === "/admin/orders" ? "admin-sidebar-active" : ""
-          }
-        >
-          <span className="icon">
-            <LayoutList />
-          </span>
-          Orders
-        </div>
-        <div
-          onClick={() => {
-            navigate("/admin/products");
-          }}
-          className={
-            location.pathname === "/admin/products"
-              ? "admin-sidebar-active"
-              : ""
-          }
-        >
-          <span className="icon">
-            <Package />
-          </span>
-          Products
-        </div>
-        <div
-          onClick={() => {
-            navigate("/admin/users");
-          }}
-          className={
-            location.pathname === "/admin/users" ? "admin-sidebar-active" : ""
-          }
-        >
-          <span className="icon">
-            <Users />
-          </span>
-          Users
+        <div className="admin-sidebar-footer">
+          <button onClick={logout}>
+            <span className="icon">
+              <LogOut size={18} />
+            </span>
+            Logout
+          </button>
         </div>
       </div>
-      <div className="admin-sidebar-footer">
-        <button onClick={logout}>
-          <span className="icon">
-            <LogOut size={18} />
-          </span>
-          Logout
-        </button>
-      </div>
-    </div>
+    </>
   );
 };
 
